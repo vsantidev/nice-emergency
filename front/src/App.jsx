15fd42166d92dd71448064ca344a73ccaf09a43c
greg-Da <gregoiredalmasso@gmail.com>
2024-11-07 21:30:10 +0100

    changed margin top

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -8,7 +8,7 @@ function App() {
 
   return (
     <>
-      <div className="flex justify-center items-center h-[100vh] flex-col">
+      <div className="mt-44 flex justify-center items-center h-[100vh] flex-col">
         <h1 className='header mb-10 text-7xl font-bold tracking-wider'>Nice <span className='mx-4'></span>Emergency</h1>
         <BtnRule />
         <div className='gameContainer relative w-[90%] max-w-[100%] h-[80%] grid grid-cols-12'>
