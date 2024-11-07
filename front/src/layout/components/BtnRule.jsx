df18c777adf4633fa91f1b9073a64a8174d8d100
vsanti <veronique.santi@epitech.eu>
2024-11-07 17:36:13 +0100

    Correction subtitle

--- front/src/layout/components/BtnRule.jsx
+++ front/src/layout/components/BtnRule.jsx
@@ -10,7 +10,7 @@ export default function BtnRule() {
             </div>
             <div>
                 <h2 className="text-3xl text-center text-gray-800 dark:text-gray-300 mb-2">
-                    Avancer dans la grille à la découverte des risques majeurs de la métropole.
+                    Aide ta métrople à avancer dans la grille à la découverte des risques majeurs.
                 </h2>
                 <div className='flex justify-center'>
                     <p className="text-2xl text-center text-gray-600 mb-12">
